9a7f3006c1446b3684fe749c5036b3908005b69a
Bartlomiej Wolk <bartlomiejwolk@gmail.com>
2015-05-29 14:04:09 +0200

    Initial commit

---
+++ Editor/LoadLevelEditor.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+
+namespace LoadLevelEx {
+
+    [CustomEditor(typeof(LoadLevel))]
+    [CanEditMultipleObjects]
+    public sealed class LoadLevelEditor : Editor {
+        #region FIELDS
+
+        private LoadLevel Script { get; set; }
+
+        #endregion FIELDS
+
+        #region SERIALIZED PROPERTIES
+
+        private SerializedProperty description;
+
+        #endregion SERIALIZED PROPERTIES
+
+        #region UNITY MESSAGES
+
+        public override void OnInspectorGUI() {
+            serializedObject.Update();
+
+            DrawVersionLabel();
+            DrawDescriptionTextArea();
+
+            EditorGUILayout.Space();
+
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable() {
+            Script = (LoadLevel)target;
+
+            description = serializedObject.FindProperty("description");
+        }
+
+        #endregion UNITY MESSAGES
+
+        #region INSPECTOR CONTROLS
+
+        private void DrawVersionLabel() {
+            EditorGUILayout.LabelField(
+                string.Format(
+                    "{0} ({1})",
+                    LoadLevel.Version,
+                    LoadLevel.Extension));
+        }
+
+        private void DrawDescriptionTextArea() {
+            description.stringValue = EditorGUILayout.TextArea(
+                description.stringValue);
+        }
+
+        #endregion INSPECTOR
+
+        #region METHODS
+
+        [MenuItem("Component/LoadLevel")]
+        private static void AddEntryToComponentMenu() {
+            if (Selection.activeGameObject != null) {
+                Selection.activeGameObject.AddComponent(typeof(LoadLevel));
+            }
+        }
+
+        #endregion METHODS
+    }
+
+}
